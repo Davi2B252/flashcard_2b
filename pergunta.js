02ca83b33940f86c38602b463aa66c227c1fcc40
Davi2B252 <davi.souza.soares@escola.pr.gov.br>
2025-10-23 07:35:47 -0300

    .

--- pergunta.js
+++ pergunta.js
@@ -1,1 +1,35 @@
+criaCartao(
+    'Devil May Cry 5',
+    'Quem é o verdadeiro nome de Urizen antes de se separar?',
+    'Vergil'
+)
 
+criaCartao(
+    'JoJo’s Bizarre Adventure',
+    'Qual é o nome do Stand de Jotaro Kujo?',
+    'Star Platinum'
+)
+
+criaCartao(
+    'JoJo’s Bizarre Adventure',
+    'Quem é o principal vilão da Parte 3: Stardust Crusaders?',
+    'DIO'
+)
+
+criaCartao(
+    'Metal Gear Rising: Revengeance',
+    'Qual é o nome completo do protagonista ciborgue do jogo?',
+    'Raiden (Jack the Ripper)'
+)
+
+criaCartao(
+    'Metal Gear Solid',
+    'Quem é o criador e líder original da unidade FOXHOUND?',
+    'Big Boss'
+)
+
+criaCartao(
+    'The Binding of Isaac',
+    'Qual é o objetivo principal do jogador em The Binding of Isaac?',
+    'Derrotar monstros e chefes em masmorras para escapar do porão'
+)
